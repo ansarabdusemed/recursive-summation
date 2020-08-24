a6bbc3f3206db8ef1a7b64645d5607033bf1a0dd
Ansar Bahrun <ansarbahrun@Ansars-MacBook-Air.local>
2020-08-23 18:12:50 -0700

    Add numbers in an array, recursively

---
+++ script.js
@@ -0,0 +1,14 @@
+function sum(arr, n) {
+    //if there are 0 elemenets in the array return nothing
+    if (n <= 0){
+      return 0;
+    }
+    //else use recursive call 
+    else {
+      //keep calling sum() until n = 0, 
+      //sum is taking the arr param & decrementing the n
+      console.log(sum(arr, n - 1) + arr[n - 1]);
+    }
+  }
+  
+sum([1, 2, 3, 4, 5,], 4);  
